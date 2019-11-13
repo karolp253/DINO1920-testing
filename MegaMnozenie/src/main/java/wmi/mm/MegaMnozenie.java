8e85ee298f307590564a8b48d9e3b373ea9ee6da
Mateusz Tylka <wirus006@gmail.com>
2019-11-13 13:49:13 +0100

    add implementation

--- MegaMnozenie/src/main/java/wmi/mm/MegaMnozenie.java
+++ MegaMnozenie/src/main/java/wmi/mm/MegaMnozenie.java
@@ -11,12 +11,23 @@ package wmi.mm;
  */
 public class MegaMnozenie {
     public static String mnozenie(String a, String b){
-        int aa = Integer.parseInt(a);
-        int bb = Integer.parseInt(b);
-        if(aa <=100 && bb <=100){
-            return Integer.toString(aa*bb);
-        } else {
-            throw new IllegalArgumentException();
+        try {
+            int aa = Integer.parseInt(a);
+            int bb = Integer.parseInt(b);
+            if(aa <=100 && bb <=100){
+                return Integer.toString(aa*bb);
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+        catch(Exception ex) {
+            double aa = Double.parseDouble(a);
+            double bb = Double.parseDouble(b);
+            if(aa <=100 && bb <=100){
+                return Double.toString(aa*bb);
+            } else {
+                throw new IllegalArgumentException();
+            }
         }
     }
 }
