7126c39191b44858722339b70509d481e0fa55d5
Marcin Kostrzewski <komar86311@gmail.com>
2019-11-13 14:40:04 +0100

    Implemented cartesian product of two non-numeric strings

--- MegaMnozenie/src/main/java/wmi/mm/MegaMnozenie.java
+++ MegaMnozenie/src/main/java/wmi/mm/MegaMnozenie.java
@@ -6,15 +6,33 @@
 package wmi.mm;
 
 /**
- *
  * @author bikol
  */
 public class MegaMnozenie {
-    public static String mnozenie(String a, String b){
+
+    static boolean isNumeric(String strNum) {
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException | NullPointerException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    public static String mnozenie(String a, String b) {
+        if (!isNumeric(a) && !isNumeric(b)) {
+            StringBuilder result = new StringBuilder();
+            for (char i : a.toCharArray())
+                for (char j : b.toCharArray())
+                    result.append(i).append(j).append(", ");
+            return result.substring(0, result.length() - 2);
+        }
+
         int aa = Integer.parseInt(a);
         int bb = Integer.parseInt(b);
-        if(aa <=100 && bb <=100){
-            return Integer.toString(aa*bb);
+
+        if (aa <= 100 && bb <= 100) {
+            return Integer.toString(aa * bb);
         } else {
             throw new IllegalArgumentException();
         }
